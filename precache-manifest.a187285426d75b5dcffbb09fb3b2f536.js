651438ca6a0e06763e08caffad1c3a63a9896d30
JH <473731589@qq.com>
2021-03-03 15:17:49 +0800

    deploy

---
+++ precache-manifest.a187285426d75b5dcffbb09fb3b2f536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33c99a8d28d4b6f9c7d",
+    "url": "/money-website/css/app.2e69cae9.css"
+  },
+  {
+    "revision": "4cad79f32a99959ee8c07c5584b78a57",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "c33c99a8d28d4b6f9c7d",
+    "url": "/money-website/js/app.867e8131.js"
+  },
+  {
+    "revision": "d29ff75d829f9cce2736",
+    "url": "/money-website/js/chunk-vendors.3ceaf735.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
